64a5535d3a511975526310febefec41c145f948d
limmens <66556410+limmens@users.noreply.github.com>
2022-08-03 13:31:32 +0200

    8: Created EntityFactory, added factory method for player components

---
+++ src/main/java/game_from_scratch/game/world/factories/EntityFactory.java
@@ -0,0 +1,20 @@
+package game_from_scratch.game.world.factories;
+
+import game_from_scratch.engine.components.*;
+
+import java.util.List;
+
+public class EntityFactory {
+    private EntityFactory(){}
+
+    public static List<Component> getPlayer() {
+        return List.of(
+                new Position(0, 0),
+                new Moving(),
+                new Rendering("player", 1),
+                new TakesInput(),
+                new Colliding(),
+                new GetsTurns(1)
+        );
+    }
+}
