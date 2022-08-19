651c36d5b0229d453d2027b12b6faa0a63e98c4e
limmens <66556410+limmens@users.noreply.github.com>
2022-08-19 13:59:18 +0200

    8: added simple test for practicing jenkins pipelines

---
+++ src/test/java/game_from_scratch/game/world/factories/EntityFactoryTest.java
@@ -0,0 +1,17 @@
+package game_from_scratch.game.world.factories;
+
+import game_from_scratch.engine.components.Component;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntityFactoryTest {
+
+    @Test
+    void getPlayerComponents() {
+        List<Component> playerComponents = EntityFactory.getPlayerComponents();
+        assertNotNull(playerComponents);
+    }
+}
